2396014c5e3bdd6cd2e7a468740013aebd87e217
Aaron Matthews <amttws@me.com>
2014-05-03 18:25:39 -0400

    setting up angular base app / routes

--- public/js/app.js
+++ public/js/app.js
@@ -3,25 +3,33 @@
 // Declare app level module which depends on filters, and services
 
 angular.module('myApp', [
-	'ngRoute',
-	'myApp.controllers',
-	'myApp.filters',
-	'myApp.services',
-	'myApp.directives'
+  'ngRoute',
+  'myApp.controllers',
+  'myApp.filters',
+  'myApp.services',
+  'myApp.directives'
 ]).
 config(function ($routeProvider, $locationProvider) {
-	$routeProvider.
-		when('/view1', {
-			templateUrl: 'partials/partial1.html',
-			controller: 'MyCtrl1'
-		}).
-		when('/view2', {
-			templateUrl: 'partials/partial2.html',
-			controller: 'MyCtrl2'
-		}).
-			otherwise({
-			redirectTo: '/view1'
-		});
+  $routeProvider.
+    when('/home', {
+      templateUrl: 'partials/home.html',
+      controller: 'HomeController'
+    }).
+    when('/dashboard', {
+      templateUrl: 'partials/dashboard.html',
+      controller: 'DashboardController'
+    }).
+    when('/new', {
+      templateUrl: 'partials/new.html',
+      controller: 'NewController'
+    }).
+    when('/view', {
+      templateUrl: 'partials/view.html',
+      controller: 'ViewController'
+    }).
+    otherwise({
+      redirectTo: '/home'
+    });
 
-	$locationProvider.html5Mode(true);
+  $locationProvider.html5Mode(true);
 });
